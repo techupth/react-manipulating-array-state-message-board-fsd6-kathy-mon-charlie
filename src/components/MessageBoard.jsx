21c6e5dcbae3c455279631183e88ad141cda9f9a
KathyKZ <kathykate.sooksutum@gmail.com>
2024-06-06 16:07:53 +0700

    feat:React Manipulating Array State Message Board Assignment homework

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,24 +1,54 @@
+import { useState } from "react";
+
 function MessageBoard() {
+  const [message, setMessage] = useState("");
+  const [fillMessage, setFillMessage] = useState([]);
+
+  const addMessege = () => {
+    const newMessage = [...fillMessage];
+    newMessage.push(message);
+    setFillMessage(newMessage);
+  };
+
+  const deleteMessage = () => {
+    const newMessage = [...fillMessage];
+    newMessage.splice(message, 1)
+    setFillMessage(newMessage);
+  };
+
   return (
     <div className="app-wrapper">
-      <h1 class="app-title">Message board</h1>
-      <div class="message-input-container">
+      <h1 className="app-title">Message board</h1>
+      <div className="message-input-container">
         <label>
           <input
             id="message-text"
             name="message-text"
             type="text"
             placeholder="Enter message here"
+            value={message}
+            onChange={(event) => {setMessage(event.target.value);
+            }}
           />
         </label>
-        <button className="submit-message-button">Submit</button>
+
+        <button className="submit-message-button" 
+        onClick={addMessege}
+        >Submit</button>
       </div>
-      <div class="board">
+      <div className="board">
+         {
+            fillMessage.map((item, index) => 
+      (
         <div className="message">
-          <h1>Hello all ! This is first message.</h1>
-          <button className="delete-button">x</button>
+          <h1>{item}</h1>
+          <button className="delete-button" 
+          onClick={() => {deleteMessage(index)}}>x</button>
         </div>
-      </div>
+      )
+            )
+          }
+          </div>
     </div>
   );
 }
