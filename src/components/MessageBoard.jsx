24210887eee29bcac1ac7f2ffec6fbf3d003636d
CharlieHii <charlie_chaper@hotmail.com>
2024-06-06 13:33:15 +0700

    feat: add message log

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,23 +1,49 @@
+import { useState } from "react";
+
 function MessageBoard() {
+  const [inputMessage, setInputMessage] = useState("");
+  const [logMessage, setLogMessage] = useState([]);
+
   return (
     <div className="app-wrapper">
-      <h1 class="app-title">Message board</h1>
-      <div class="message-input-container">
+      <h1 className="app-title">Message board</h1>
+      <div className="message-input-container">
         <label>
           <input
             id="message-text"
             name="message-text"
             type="text"
             placeholder="Enter message here"
+            value={inputMessage}
+            onChange={(event) => {
+              setInputMessage(event.target.value);
+            }}
           />
         </label>
-        <button className="submit-message-button">Submit</button>
+        <button
+          className="submit-message-button"
+          onClick={() => {
+            inputMessage
+              ? setLogMessage([...logMessage, inputMessage])
+              : alert("Please submit text");
+            setInputMessage("");
+          }}
+        >
+          Submit
+        </button>
       </div>
-      <div class="board">
-        <div className="message">
-          <h1>Hello all ! This is first message.</h1>
-          <button className="delete-button">x</button>
-        </div>
+      <div className="board">
+        {logMessage.map((item, index) => (
+          <div className="message" key={index}>
+            <h1>{item}</h1>
+            <button
+              className="delete-button"
+              onClick={() => setLogMessage(logMessage.toSpliced(index, 1))}
+            >
+              x
+            </button>
+          </div>
+        ))}
       </div>
     </div>
   );
